cd6292ecb3efd1f479c37ec60f28e417587b3476
Zhang Tianzhu <tianzhu.zhang@nokia.com>
2022-01-11 23:29:55 +0100

    update results

--- data/SFC/perf.sh
+++ data/SFC/perf.sh
@@ -44,7 +44,7 @@ sudo timeout ${time_out} taskset -c 1 perf stat -e instructions,branches,branch-
 sudo timeout ${time_out} taskset -c 1 perf stat -e instructions,branches,branch-misses,branch-load-misses,cache-misses,cache-references,cycles,context-switches,cpu-clock,minor-faults,page-faults,task-clock,bus-cycles,ref-cycles,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,LLC-load-misses,LLC-store-misses,LLC-stores,LLC-loads,dTLB-stores,dTLB-load-misses,dTLB-store-misses,iTLB-loads,iTLB-load-misses,node-load-misses,node-loads,node-store-misses,node-stores \
          -x, -o "${sfc}-vnf/${sfc}-vnf-independent-core-freq-${freq}-all-fastclick.txt" -r 1 -p ${click_all_pid} -I ${interval}
 
-sudo timeout ${time_out} taskset -c 1 perf stat -e instructions,branches,branch-misses,branch-load-misses,cache-misses,cache-references,cycles,context-switches,cpu-clock,minor-faults,page-faults,task-clock,bus-cycles,ref-cycles,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,LLC-load-misses,LLC-store-misses,LLC-stores,LLC-loads,dTLB-stores,dTLB-load-misses,dTLB-store-misses,iTLB-loads,iTLB-load-misses,node-load-misses,node-loads,node-store-misses,node-stores \
-         -x, -o "${sfc}-vnf/${sfc}-vnf-independent-core-freq-${freq}-single-fastclick.txt" -r 1 -p ${click_single_pid} -I ${interval}
+#sudo timeout ${time_out} taskset -c 1 perf stat -e instructions,branches,branch-misses,branch-load-misses,cache-misses,cache-references,cycles,context-switches,cpu-clock,minor-faults,page-faults,task-clock,bus-cycles,ref-cycles,L1-dcache-load-misses,L1-dcache-loads,L1-dcache-stores,L1-icache-load-misses,LLC-load-misses,LLC-store-misses,LLC-stores,LLC-loads,dTLB-stores,dTLB-load-misses,dTLB-store-misses,iTLB-loads,iTLB-load-misses,node-load-misses,node-loads,node-store-misses,node-stores \
+#         -x, -o "${sfc}-vnf/${sfc}-vnf-independent-core-freq-${freq}-single-fastclick.txt" -r 1 -p ${click_single_pid} -I ${interval}
 
 done
